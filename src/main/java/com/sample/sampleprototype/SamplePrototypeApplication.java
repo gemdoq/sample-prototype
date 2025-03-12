854ba50a50bbdc967c9c8ecc0f4b63ab68f9e892
SeungHo <gemdoq@gmail.com>
2025-03-13 01:11:24 +0900

    1st commit

---
+++ src/main/java/com/sample/sampleprototype/SamplePrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.sample.sampleprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplePrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplePrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/sampleprototype/SamplePrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.sampleprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplePrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
