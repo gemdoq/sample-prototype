ba2bca46223ed0ee2afe50d4a6c626ad82676b8a
SeungHo <gemdoq@gmail.com>
2025-03-19 06:02:04 +0900

    feat(api): create test api for cicd

--- src/main/java/com/sample/sampleprototype/SamplePrototypeApplication.java
+++ src/main/java/com/sample/sampleprototype/SamplePrototypeApplication.java
@@ -2,12 +2,20 @@ package com.sample.sampleprototype;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SamplePrototypeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SamplePrototypeApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, CI/CD!";
 	}
 
 }
