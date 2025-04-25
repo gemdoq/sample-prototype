e7d7271e85a5aa78f0784f3272019313468aa84b
SeungHo <gemdoq@gmail.com>
2025-04-26 02:54:57 +0900

    feat(audit): JPA Auditing을 사용하도록 설정하고, audit을 적용할 기초 엔티티 정의

---
+++ src/main/java/com/sample/prototype/common/model/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.sample.prototype.common.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime createdDttm;
+
+	@LastModifiedDate
+	private LocalDateTime updatedDttm;
+}
