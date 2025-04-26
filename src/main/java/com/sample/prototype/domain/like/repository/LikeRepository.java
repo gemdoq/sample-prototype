fe7b98d474e49048c6015ad290aa630e81dfee25
SeungHo <gemdoq@gmail.com>
2025-04-26 09:54:04 +0900

    feat(like): 좋아요 정보 JPA 리포지토리 생성 및 기존 게시글 JPA 리포지토리의 메서드 변경
    - BoardRepository.java: 오버라이드 된 findById 메서드의 EntityGraph어노테이션의 속성인 attributePaths에 좋아요 조건 추가

---
+++ src/main/java/com/sample/prototype/domain/like/repository/LikeRepository.java
@@ -0,0 +1,7 @@
+package com.sample.prototype.domain.like.repository;
+
+import com.sample.prototype.domain.like.model.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+}
