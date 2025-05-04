0b53fdaad6259c2554951ecb49c25aa10c5f2cf0
SeungHo <gemdoq@gmail.com>
2025-05-04 19:46:35 +0900

    feat(user): 사용자 조회 기능 추가

--- src/main/java/com/sample/prototype/domain/user/UserRepository.java
+++ src/main/java/com/sample/prototype/domain/user/UserRepository.java
@@ -3,5 +3,10 @@ package com.sample.prototype.domain.user;
 import com.sample.prototype.domain.user.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByUsername(String username);
+
+	Optional<User> findByEmail(String email);
 }
