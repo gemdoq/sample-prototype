f71a44e25820935845663c365ec5d8d80cd75539
SeungHo <gemdoq@gmail.com>
2025-05-16 13:36:16 +0900

    feat(comment): 댓글 요청/응답 DTO 클래스 추가
    - 댓글 작성 시 사용할 CommentRequestDTO에 유효성 검사(@NotBlank) 적용
    - 댓글 조회 시 응답용 CommentResponseDTO에 작성자 및 작성일시 포함

---
+++ src/main/java/com/sample/prototype/domain/comment/model/dto/CommentResponseDTO.java
@@ -0,0 +1,15 @@
+package com.sample.prototype.domain.comment.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class CommentResponseDTO {
+	private Long id;
+	private String content;
+	private String author;
+	private LocalDateTime createdDttm;
+}
