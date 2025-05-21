3a3335d544d25e109dcd9c9e5fae009e0d39bd18
SeungHo <gemdoq@gmail.com>
2025-05-21 10:26:08 +0900

    refactor(comment): 기존 `HTML view를 반환하던 Controller`에서 다루던 JSON데이터에 대한 처리를 `REST API를 다루는 RestController`와 `Service`로 분리시키면서 그에 따라 comment 도메인 관련 리팩토링
    - 수정)CommentResponseDTO: 댓글 응답을 위한 DTO
    - 수정)CommentRepository: 게시글로 관련 댓글 목록을 조회하는 메서드 생성
    - 신규)CommentService: 기존 비즈니스 로직 관련 메서드 옮김
    - 신규)CommentRestController: REST API에 대한 요청을 처리하는 컨트롤러
    - 수정)admin-comments: 관리자 댓글 목록을 API 기반 동적 렌더링 및 Axios 처리 방식으로 변경
    - 수정)comment-form: 댓글 작성 방식을 form POST에서 Axios 비동기 요청으로 변경

--- src/main/java/com/sample/prototype/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/sample/prototype/domain/comment/repository/CommentRepository.java
@@ -2,6 +2,11 @@ package com.sample.prototype.domain.comment.repository;
 
 import com.sample.prototype.domain.comment.model.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+	List<Comment> findAllByBoardId(Long boardId);
 }
