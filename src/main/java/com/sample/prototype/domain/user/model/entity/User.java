27bdaefb1999eca078cdde22b7dbe83babba48e0
SeungHo <gemdoq@gmail.com>
2025-06-08 12:00:00 +0900

    feat(validation): User 엔티티에 유효성 검사 어노테이션 추가

--- src/main/java/com/sample/prototype/domain/user/model/entity/User.java
+++ src/main/java/com/sample/prototype/domain/user/model/entity/User.java
@@ -2,7 +2,12 @@ package com.sample.prototype.domain.user.model.entity;
 
 import com.sample.prototype.common.model.entity.BaseEntity;
 import com.sample.prototype.domain.user.model.enums.Role;
+import com.sample.prototype.global.validation.ValidEmailDomain;
+import com.sample.prototype.global.validation.ValidUsername;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,12 +21,19 @@ public class User extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotBlank(message = "사용자 이름은 필수입니다.")
+	@ValidUsername
 	@Column(nullable = false, unique = true)
 	private String username;
 
+	@NotBlank(message = "이메일은 필수입니다.")
+	@Email(message = "유효한 이메일 형식이어야 합니다.")
+	@ValidEmailDomain
 	@Column(nullable = false, unique = true)
 	private String email;
 
+	@NotBlank(message = "비밀번호는 필수입니다.")
+	@Size(min = 6, max = 100, message = "비밀번호는 6~100자여야 합니다.")
 	@Column(nullable = false)
 	private String password;
 
