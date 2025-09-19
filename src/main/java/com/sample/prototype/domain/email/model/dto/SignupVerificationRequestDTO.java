548050a842bb09b2b109c833e880b7f5576803e0
SeungHo <gemdoq@gmail.com>
2025-09-19 12:00:00 +0900

    feat(mail/signup): SignupVerificationRequestDTO 추가
    - 회원가입 완료 요청 DTO
    - username, email, password 필드 및 유효성 검증

---
+++ src/main/java/com/sample/prototype/domain/email/model/dto/SignupVerificationRequestDTO.java
@@ -0,0 +1,32 @@
+package com.sample.prototype.domain.email.model.dto;
+
+import com.sample.prototype.global.validation.ValidEmailDomain;
+import com.sample.prototype.global.validation.ValidUsername;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 회원가입 인증 요청 DTO
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class SignupVerificationRequestDTO {
+
+	@NotBlank(message = "사용자 이름은 필수입니다.")
+	@ValidUsername
+	private String username;
+
+	@NotBlank(message = "이메일은 필수입니다.")
+	@Email(message = "올바른 이메일 형식이 아닙니다.")
+	@ValidEmailDomain
+	private String email;
+
+	@NotBlank(message = "비밀번호는 필수입니다.")
+	@Size(min = 8, max = 20, message = "비밀번호는 8~20자여야 합니다.")
+	private String password;
+}
