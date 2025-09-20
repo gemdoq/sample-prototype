d811270f60ce3025f9d6163ded8fc7e2e0b64eec
SeungHo <gemdoq@gmail.com>
2025-09-20 12:00:00 +0900

    feat(mail/signup): VerifyCodeRequestDTO 추가
    - 인증번호 확인 요청 DTO
    - email, code 필드 및 유효성 검증

---
+++ src/main/java/com/sample/prototype/domain/email/model/dto/VerifyCodeRequestDTO.java
@@ -0,0 +1,27 @@
+package com.sample.prototype.domain.email.model.dto;
+
+import com.sample.prototype.global.validation.ValidEmailDomain;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 인증번호 확인 요청 DTO
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class VerifyCodeRequestDTO {
+
+	@NotBlank(message = "이메일은 필수입니다.")
+	@Email(message = "올바른 이메일 형식이 아닙니다.")
+	@ValidEmailDomain
+	private String email;
+
+	@NotBlank(message = "인증번호는 필수입니다.")
+	@Size(min = 6, max = 6, message = "인증번호는 6자리입니다.")
+	private String code;
+}
