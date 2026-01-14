8b8f817000c64da83ce457c8aeff14ee51c939cc
SeungHo <gemdoq@gmail.com>
2026-01-14 17:10:20 +0900

    feat(share): 공유 REST API 및 리다이렉트 컨트롤러 추가
    - ShareRestController: /api/shares/boards/{id}/link 엔드포인트
    - ShareRedirectController: /s/{shortCode} 단축 URL 리다이렉트
    - Swagger 문서화 적용

---
+++ src/main/java/com/sample/prototype/domain/share/controller/ShareRedirectController.java
@@ -0,0 +1,44 @@
+package com.sample.prototype.domain.share.controller;
+
+import com.sample.prototype.domain.share.model.entity.ShareLink;
+import com.sample.prototype.domain.share.repository.ShareLinkRepository;
+import com.sample.prototype.global.exception.AppException;
+import com.sample.prototype.global.exception.Domain;
+import com.sample.prototype.global.exception.ErrorCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * 단축 URL 리다이렉트 컨트롤러
+ * /s/{shortCode} 요청을 /boards/{boardId}로 리다이렉트함
+ */
+@Controller
+public class ShareRedirectController {
+
+	private static final Logger log = LoggerFactory.getLogger(ShareRedirectController.class);
+
+	private final ShareLinkRepository shareLinkRepository;
+
+	public ShareRedirectController(ShareLinkRepository shareLinkRepository) {
+		this.shareLinkRepository = shareLinkRepository;
+	}
+
+	@GetMapping("/s/{shortCode}")
+	public String redirectToBoard(@PathVariable String shortCode) {
+		log.debug("단축 URL 리다이렉트 요청: shortCode={}", shortCode);
+
+		ShareLink shareLink = shareLinkRepository.findByShortCode(shortCode)
+				.orElseThrow(() -> new AppException(
+						Domain.BOARD,
+						ErrorCode.BOARD_NOT_FOUND,
+						"존재하지 않는 공유 링크입니다: " + shortCode
+				));
+
+		log.debug("리다이렉트 대상: boardId={}", shareLink.getBoardId());
+
+		return "redirect:/boards/" + shareLink.getBoardId();
+	}
+}
